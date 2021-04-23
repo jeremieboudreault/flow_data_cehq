a85fbda4e91ed379c21218b059af2f4e5f23b939
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-23 09:39:24 -0400

    Initial commit on read_table(), which read the underlying table in a CEHQ file.

---
+++ R/functions/read_table.R
@@ -0,0 +1,31 @@
+# read_table.R
+
+
+# Read the table of the file and return a data.table::data.table().
+
+
+read_table <- function(path) {
+
+    # Read the file.
+    x <- data.table::fread(
+        input = path,
+        skip  = 21L,
+        fill  = TRUE
+    )
+
+    dim(x)
+
+    # Updates names.
+    names(x) <- c("STATION", "DATE", "FLOW", "FLAG", "EMPTY")
+
+    # Drop not relevent columns.
+    x[, `:=`(STATION = NULL, EMPTY = NULL)]
+
+    # Convert data to integer format.
+    x[, DATE := date_to_int(DATE)]
+
+    # Return the table.
+    return(x[])
+
+}
+
