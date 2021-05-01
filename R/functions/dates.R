f67ea28196bc49739e4ea217a53b7d7304fb7db9
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-05-01 11:41:48 -0400

    Added a function to check for leap year (is_leap()) in dates.R.

--- R/functions/dates.R
+++ R/functions/dates.R
@@ -1,6 +1,16 @@
 # dates.R
 
 
+# Simple dates functions to used within this project.
+
+
+# Project : flow_data_cehq
+# Author  : Jeremie Boudreault
+# Email   : jeremieboudreault11 [at] gmail [dot] com
+# Depends : R (v3.6.3)
+# License : To be determined...
+
+
 # Transform a date format into the integer one.
 date_to_int <- function(x) {
     x <- as.character(x)
@@ -17,4 +27,9 @@ int_to_date <- function(x) {
     mm <- substr(x, 5L, 6L)
     dd <- substr(x, 7L, 8L)
     return(as.Date(paste0(yy, "/", mm, "/", dd)))
+}
+
+# Check for leap year.
+is_leap <- function(x) {
+    return(as.integer(((x %% 4L) == 0L)))
 }
