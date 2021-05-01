7b8c8cb8b0c85a1a7f081438774a788947160400
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-05-01 10:15:47 -0400

    Updated rvest_stations.R. Few modifications : - Returned the final table as a data.table(). - Rewrote some small part of the code. - Added some spacing. - Renamed the function to fetch_stns_numbers().

--- R/functions/rvest_stations.R
+++ R/functions/rvest_stations.R
@@ -1,41 +1,64 @@
-# rvest_stations.R
+# fetch_stns_numbers.R
 
 
-# Get station numbers within a given hydrographic region
+# Get station numbers within a given hydrographic region.
 
-# requires rvest, dplyr
-# weird to put that in there right?
-library(dplyr)
-library(rvest)
 
-rvest_stations <- function(
+# Project : flow_data_cehq
+# Author  : Samuel Perreault
+# Email   : sperreault2407 [at] gmail [doc] com
+# Depends : R (v3.6.3)
+# License : To be determined...
+
+
+fetch_stns_numbers <- function(
     region_numbers # 0 to 13 (except 0 and 11)
 ) {
 
+    # Convert to character.
     region_numbers <- as.character(region_numbers)
-    for(k in seq_along(region_numbers)){
-        if(nchar(region_numbers[k]) == 1) region_numbers[k] <- paste0("0",region_numbers[k])
-    }
 
-    # url of interest (April 21, 2021)
-    urls <- paste0("https://www.cehq.gouv.qc.ca/hydrometrie/historique_donnees/ListeStation.asp?regionhydro=",region_numbers,"&Tri=Non")
+    # Loop on all regions numbers.
+    region_numbers <- lapply(
+        X   = region_numbers,
+        FUN = function(w) {
+            if (nchar(w) == 1L) {
+                return(paste0("0", w))
+            } else {
+                return(w)
+            }
+        }
+    )
 
-    meta_list <- lapply(urls, function(url){
-        read_html(url) %>%
-            html_node("#tblListe") %>%
-            html_table()
+    # Url of interest (as of April 21, 2021).
+    urls <- paste0(
+        "https://www.cehq.gouv.qc.ca/hydrometrie/historique_donnees/",
+        "ListeStation.asp?regionhydro=", region_numbers, "&Tri=Non"
+    )
+
+    # Extract list of station in the table.
+    meta_list <- lapply(urls, function(url) {
+        read_html(url)         %>%
+        html_node("#tblListe") %>%
+        html_table()
     })
 
-    # first row of each tibble contains the column names, we edit them to avoid later problems
-    meta_title <- meta_list[[1]][1,]
-    meta_title <- substr(meta_title,1,sapply(meta_title,nchar)-1)
+    # First row of each tibble contains the column names, we edit them to avoid later problems
+    meta_title <- meta_list[[1L]][1L, ]
+    meta_title <- substr(meta_title, 1L, sapply(meta_title, nchar) - 1L)
     meta_title <- gsub(" ", "_", meta_title)
 
     # rbind all tibbles (without first row) and set names
-    stns_meta <- do.call("rbind", lapply(meta_list, function(tab) tab[-1,,drop=F]))
+    stns_meta <- do.call(
+        what = "rbind",
+        args = lapply(meta_list, function(tab) tab[-1L, , drop = FALSE])
+    )
+
+    # Update names.
     names(stns_meta) <- meta_title
 
-    stns_meta
-}
+    # Return final table as a data.table.
+    return(data.table::setDT(stns_meta))
 
+}
 
