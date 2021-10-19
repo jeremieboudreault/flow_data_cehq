62ea8b79bdf2260ff7f3218425eaa41a4c52f319
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-10-19 09:55:22 -0400

    Added a path argument to the download_stns_files() function.

--- R/functions/download_stns_files.R
+++ R/functions/download_stns_files.R
@@ -16,16 +16,17 @@ download_stns_files <- function(
     suffix  = "Q",   # Looking for _Q.txt files (or _N.txt files?), can be a vector
     replace = TRUE,  # replace existing files (or skip download)
     wait    = 1L,
-    verbose = TRUE
+    verbose = TRUE,
+    path    = file.path("data")
 ) {
 
     # Filenames (as on the cehq website)
     filenames <- paste0(station_numbers, "_", suffix, ".txt")
 
     # Check if some already exist
-    existing_files <- intersect(list.files("data"), filenames)
+    existing_files <- intersect(list.files(path), filenames)
 
-    if(replace & length(existing_files) > 0) file.remove(file.path("data", existing_files))
+    if(replace & length(existing_files) > 0) file.remove(file.path(path, existing_files))
     if(!replace & length(existing_files) > 0){
         filenames <- setdiff(filenames, existing_files)
         if(length(filenames) == 0 & verbose){
@@ -45,14 +46,14 @@ download_stns_files <- function(
         urls <- paste0("https://www.cehq.gouv.qc.ca/depot/historique_donnees/fichier/",filenames)
 
         # write file with urls in it
-        write.table(data.frame(urls), file.path("data","temp_station_urls00.txt"), row.names = F, col.names = F, quote = F)
+        write.table(data.frame(urls), file.path(path,"temp_station_urls00.txt"), row.names = F, col.names = F, quote = F)
 
         # run command in terminal
         shell_command <- paste0("cd data; wget --input-file temp_station_urls00.txt --wait=",wait)
         system(command = shell_command)
 
         # remove temp file
-        file.remove(file.path("data","temp_station_urls00.txt"))
+        file.remove(file.path(path, "temp_station_urls00.txt"))
 
     } else {
         cat("Implemented only for Linux and Mac OS (so far).\n")
